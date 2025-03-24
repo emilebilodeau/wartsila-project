b94488b674e9e836528847dd3b130c951888fc13
emilebilodeau <emilebilodeau@gmail.com>
2025-03-24 10:09:25 -0700

    added navbar basic functionality and pages home, form, and data

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./pages/home/home.component').then((m) => m.HomeComponent);
+    },
+  },
+  {
+    path: 'form',
+    loadComponent: () => {
+      return import('./pages/form/form.component').then((m) => m.FormComponent);
+    },
+  },
+  {
+    path: 'data',
+    loadComponent: () => {
+      return import('./pages/data/data.component').then((m) => m.DataComponent);
+    },
+  },
+];
